5f22680b765f40ca931ab0f4e76191f9ab488694
Leandro Medeiros <leandrotinfo19@gmail.com>
2017-05-30 14:11:29 -0300

    adicionando fontes e imagens

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,27 @@
-// $(document).ready(function(){
-//   $('.bxslider').bxSlider();
-// });
+$(document).ready(function(){
+  $('.slider-header').bxSlider({
+    controls: false
+  });
+
+  $('.slider-projects').bxSlider({
+    slideWidth: 300,
+    minSlides: 3,
+    maxSlides: 3,
+    slideMargin: 10,
+    pager: false,
+    nextText: '>',
+    prevText: '<'
+  });
+
+  $('.slider-store-window').bxSlider({
+    slideWidth: 128,
+    minSlides: 4,
+    maxSlides: 4,
+    slideMargin: 10,
+    pager: false,
+    nextText: '>',
+    prevText: '<'
+  });
+
+  
+});
